d675fd0cd2ff28eeb0b189c381b9f7ab25ddb06a
xietanu <sutton.matt.p@gmail.com>
2022-04-30 22:30:32 +0100

    Fix missing namespace imports

--- src/arcade_sprite_ext/__init__.py
+++ src/arcade_sprite_ext/__init__.py
@@ -1,3 +1,4 @@
 """Simplify imports for the base of the package"""
 from .multi_part_sprite import MultiPartSprite
 from .coords import Coords
+from .z_height_mixin import ZHeightMixin, ZSortedSpriteList, SpriteWithZHeight, ZHeightSortMixin
