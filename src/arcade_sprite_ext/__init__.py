31ba9451216e914e37b9d4f0c08a34f3b781324a
xietanu <sutton.matt.p@gmail.com>
2022-05-10 18:58:58 +0100

    Fix namespace error

--- src/arcade_sprite_ext/__init__.py
+++ src/arcade_sprite_ext/__init__.py
@@ -1,5 +1,5 @@
 """Simplify imports for the base of the package."""
-import tools
+from . import tools
 from .multi_part_sprite import MultiPartSprite
 from .coords import Coords
 from .z_height_mixin import (
